c05fad6111817fffe7843c0cedc61d86adb37422
LAURENT KNAUSS <laurentknauss@gmail.com>
2022-09-21 15:20:56 +0200

    Add Files

---
+++ Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.13;
+
+constract Constants { 
+  // Coding convention to uppercase constant variables
+    addres public constant MY_ADDRESS = 0x..............;
+    uint public constant MY_UINT = 111;
+}
